97eb2b5f0e2ba999dae236ce351e000480525d5b
Diamond <jinxiu3579@163.com>
2020-08-08 12:38:30 +0800

    init project

---
+++ src/test/java/com/example/forumback/ForumBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forumback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
